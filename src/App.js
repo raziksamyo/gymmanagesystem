27413b5e6e907ab317146672c291aaa6485313ea
Madhur <madhur@192.168.1.29>
2023-01-25 12:05:57 +0530

    Fixed Code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Schedule from "./Component/Schedule";
 import Contact from "./Component/Contact";
 import Footer from "./Component/Footer";
 import Classes from "./Component/Classes";
-import AdminHome from "./Admin/AdminHome";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
       <Schedule />
       <Contact />
       <Footer />
-      {/* <AdminHome /> */}
     </>
   );
 }
